96330f43d49e832b6f7c6cbb4c31b6e1aa34c98f
Anton <lelfhtd1998@gmail.com>
2026-08-12 19:43:29 +0300

    TASK-1002: Изменил название пакета на rhealth и опустил ошибку

--- internal/app/handler/http/health/health.go
+++ internal/app/handler/http/health/health.go
@@ -1,7 +1,6 @@
-package health
+package rhealth
 
 import (
-	"log"
 	"net/http"
 
 	rhandler "github.com/Pak3n/catalog-service/internal/app/handler/http"
@@ -14,8 +13,5 @@ func NewHandler() rhandler.Health {
 }
 
 func (h *handler) LastCheck(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("ok")); err != nil {
-		log.Printf("Failed to write health response: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
+	_, _ = w.Write([]byte("ok"))
 }

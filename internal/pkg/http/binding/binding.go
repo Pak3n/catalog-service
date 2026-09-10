915e9d73d15c280a75e32c49fb2b98e3a5c4beb2
Anton <lelfhtd1998@gmail.com>
2026-09-10 22:56:19 +0300

    TASK-1006: Binding и валидация

---
+++ internal/pkg/http/binding/binding.go
@@ -0,0 +1,45 @@
+package binding
+
+import (
+	"net/http"
+)
+
+const (
+	MIMEJSON              = "application/json"
+	MIMEPOSTForm          = "application/x-www-form-urlencoded"
+	MIMEMultipartPOSTForm = "multipart/form-data"
+)
+
+type Binding interface {
+	Name() string
+	Bind(r *http.Request, obj any) error
+}
+
+type BindingUri interface {
+	Name() string
+	BindUri(params map[string][]string, obj any) error
+}
+
+type StructValidator interface {
+	ValidateStruct(obj any) error
+	Engine() any
+}
+
+var Validator StructValidator = &defaultValidator{}
+
+func (v *defaultValidator) Engine() any {
+	v.lazyInit()
+	return v.validate
+}
+
+var (
+	bJSON  = jsonBinding{}
+	bQuery = queryBinding{}
+)
+
+func validate(obj any) error {
+	if err := Validator.ValidateStruct(obj); err != nil {
+		return &bindingError{msg: err.Error()}
+	}
+	return nil
+}
